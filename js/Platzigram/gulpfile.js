064867d54adde9cb2116734f846dd83c2d7df35d
Robbie Reta <robbie.reta67@gmail.com>
2017-03-06 19:19:51 -0600

    automatizacion con gulp

--- js/Platzigram/gulpfile.js
+++ js/Platzigram/gulpfile.js
@@ -4,6 +4,7 @@ var rename=require('gulp-rename');
 var babel = require("babelify");
 var browserify = require("browserify");
 var source= require('vinyl-source-stream');
+var watchify= require('watchify');
 
 gulp.task('styles',function(){
   gulp
@@ -19,13 +20,32 @@ gulp.task('assets',function(){
     .pipe(gulp.dest('public'));
 
 })
-gulp.task('scripts', function () {
-  browserify('./src/index.js')
+
+function compile(watch){
+  var bundle=watchify(browserify('./src/index.js',{debug:true}));
+function rebundle(){
+  bundle
     .transform(babel, {presets: ["es2015"]})
     .bundle()
     .pipe(source('index.js'))
     .pipe(rename('app.js'))
     .pipe(gulp.dest('public'));
+}
+  if(watch){
+    bundle.on('update',function(){
+      console.log('--> Bundling...');
+      rebundle();
+    });
+
+  }
+  rebundle();
+}
+
+gulp.task('build',function(){
+  return compile();
 });
 
-gulp.task('default',['styles','assets','scripts'])
+gulp.task('watch',function(){
+  return compile(true);
+});
+gulp.task('default',['styles','assets','build'])
